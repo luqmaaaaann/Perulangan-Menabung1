5640cafd5627cd9febe2ad51b613690382d22d40
luqmaaaaann <49086957+luqmaaaaann@users.noreply.github.com>
2021-04-02 21:35:31 +0700

    Update MariMenabung1.java

--- MariMenabung1.java
+++ MariMenabung1.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package marimenabung1;
 
-/**
- *
- * @author Inue
- */
 import java.util. *;
 public class MariMenabung1 {
 
